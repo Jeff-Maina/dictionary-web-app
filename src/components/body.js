89c62eeb491aa503a672eedfc52db98da2e63780
Jeff <jeffgichuki68@gmail.com>
2023-02-20 23:09:06 +0300

    removed duplicate word types

--- src/components/body.js
+++ src/components/body.js
@@ -2,47 +2,76 @@ import { useState } from "react";
 import "../styles/body.css";
 
 function Body({ wordData, isPending }) {
-    console.log(wordData);
-
   let [isPlaying, setIsPlaying] = useState(false);
+  let [wordType, setWordType] = useState("noun");
+  let [dataRendered, setDataRendered] = useState("verb");
 
-  let nouns_arr = [];
+  function handleRender(value) {
+    console.log(value);
+    setWordType(value);
+  }
+
+  let definitions_arr = [];
   let verbs_arr = [];
   let adjective_arr = [];
   let adverb_arr = [];
 
-  let nouns_list = wordData.filter((word) => {
-    if (word.meanings[0].partOfSpeech === "noun") {
-      nouns_arr.push(word.meanings[0]);
-      // if (nouns_arr.length > 0) {
-      //     setIsNoun(false);
-      //     }
+  let definitions_list = wordData.filter((word) => {
+    if (word.meanings[0].partOfSpeech === wordType) {
+      definitions_arr.push(word.meanings[0]);
     }
-    return nouns_arr;
   });
 
-  let verbs_list = wordData.filter((word) => {
-    if (word.meanings[0].partOfSpeech === "verb") {
-      verbs_arr.push(word.meanings[0]);
-    }
-    return nouns_arr;
+  let arr = [];
+
+  let firstType = wordData.map((word) => {
+    return arr.push(word.meanings[0].partOfSpeech);
   });
 
-  let adj_list = wordData.filter((word) => {
-    if (word.meanings[0].partOfSpeech === "adjective") {
-      adjective_arr.push(word.meanings[0]);
-    }
-    return nouns_arr;
+  let uniqueArray = removeDuplicates(arr);
+
+  let wordTypeList = uniqueArray.map((word) => {
+    return (
+      <h3
+        onClick={() => {
+          handleRender(word);
+        }}
+        id="syn"
+      >
+        {" "}
+        {word} |
+      </h3>
+    );
   });
 
-  let adverb_list = wordData.filter((word) => {
-    if (word.meanings[0].partOfSpeech === "adverb") {
-      adverb_arr.push(word.meanings[0]);
-    }
-    return nouns_arr;
-  });
+  function removeDuplicates(arr) {
+    return Array.from(new Set(arr));
+  }
 
-  let noun_definitions = nouns_arr.map((word) => {
+  console.log(uniqueArray);
+
+  // let verbs_list = wordData.filter((word) => {
+  //   if (word.meanings[0].partOfSpeech === "verb") {
+  //     verbs_arr.push(word.meanings[0]);
+  //   }
+  //   return nouns_arr;
+  // });
+
+  // let adj_list = wordData.filter((word) => {
+  //   if (word.meanings[0].partOfSpeech === "adjective") {
+  //     adjective_arr.push(word.meanings[0]);
+  //   }
+  //   return nouns_arr;
+  // });
+
+  // let adverb_list = wordData.filter((word) => {
+  //   if (word.meanings[0].partOfSpeech === "adverb") {
+  //     adverb_arr.push(word.meanings[0]);
+  //   }
+  //   return nouns_arr;
+  // });
+
+  let words_definitions = definitions_arr.map((word) => {
     return word.definitions.map((def) => {
       return (
         <li>
@@ -52,35 +81,35 @@ function Body({ wordData, isPending }) {
     });
   });
 
-  let verb_definitions = verbs_arr.map((word) => {
-    return word.definitions.map((def) => {
-      return (
-        <li>
-          <span>{def.definition}</span>
-        </li>
-      );
-    });
-  });
+  // let verb_definitions = verbs_arr.map((word) => {
+  //   return word.definitions.map((def) => {
+  //     return (
+  //       <li>
+  //         <span>{def.definition}</span>
+  //       </li>
+  //     );
+  //   });
+  // });
 
-  let adjective_definitions = adjective_arr.map((word) => {
-    return word.definitions.map((def) => {
-      return (
-        <li>
-          <span>{def.definition}</span>
-        </li>
-      );
-    });
-  });
+  // let adjective_definitions = adjective_arr.map((word) => {
+  //   return word.definitions.map((def) => {
+  //     return (
+  //       <li>
+  //         <span>{def.definition}</span>
+  //       </li>
+  //     );
+  //   });
+  // });
 
-  let adverb_definitions = adverb_arr.map((word) => {
-    return word.definitions.map((def) => {
-      return (
-        <li>
-          <span>{def.definition}</span>
-        </li>
-      );
-    });
-  });
+  // let adverb_definitions = adverb_arr.map((word) => {
+  //   return word.definitions.map((def) => {
+  //     return (
+  //       <li>
+  //         <span>{def.definition}</span>
+  //       </li>
+  //     );
+  //   });
+  // });
 
   let playState = isPlaying ? "pause" : "play_arrow";
 
@@ -102,7 +131,7 @@ function Body({ wordData, isPending }) {
         </div>
       )}
       {isPending || (
-        <>
+        <section>
           <div className="word">
             <h2>{wordData[0].word}</h2>
             <h3 id="phonetic">{wordData[0].phonetic}</h3>
@@ -113,18 +142,16 @@ function Body({ wordData, isPending }) {
             </button>
           </div>
           <div className="meaning">
+            <span id="synonyms">{wordTypeList}</span>
             <span className="span">
-              <h3 id="noun-title">noun</h3>
+              <h3 id="noun-title">{wordType}</h3>
               <hr />
             </span>
             <h3 id="meaning-title">Meaning</h3>
           </div>
-          <ul className="list noun">{noun_definitions}</ul>
-          <span id="synonyms">
-            <h3>Synonyms</h3>
-            <h3 id="syn">Electric guitar</h3>
-          </span>
-          <span className="span">
+          <ul className="list noun">{words_definitions}</ul>
+
+          {/* <span className="span">
             <h3 id="noun-title">verb</h3>
             <hr />
           </span>
@@ -141,8 +168,8 @@ function Body({ wordData, isPending }) {
             <hr />
           </span>
           <h3 id="meaning-title">Meaning</h3>
-          <ul className="list verb">{adverb_definitions}</ul>
-        </>
+          <ul className="list verb">{adverb_definitions}</ul> */}
+        </section>
       )}
     </main>
   );
